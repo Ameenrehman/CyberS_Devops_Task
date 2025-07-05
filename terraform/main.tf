6a49cd7a20cea73d0985e12d613899f2e8b01d88
Ameenrehman <ameen2607@gmail.com>
2025-07-05 21:41:20 +0530

    trying new main.tf for eks

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,20 +8,49 @@
 
 } */
 
+# main.tf
+
+# Pin the AWS provider version for compatibility with modules
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # Pin to a compatible major version
+    }
+  }
+  backend "s3" {
+    # These values will be overridden by -backend-config in CI/CD
+    region = "us-east-1"
+    key    = "tfstate/main.tfstate"
+  }
+}
 
 provider "aws" {
   region = local.region
 }
 
+# Configure the Kubernetes provider to connect to the EKS cluster
+# This uses outputs from the EKS module to get the cluster endpoint and CA certificate.
+# It also uses the aws_eks_cluster_auth data source to get a temporary authentication token.
+data "aws_eks_cluster_auth" "main" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.main.token
+}
+
 locals {
-  name   = "ascode-cluster"
+  name = "ascode-cluster"
   region = "us-east-1"
 
   vpc_cidr = "10.123.0.0/16"
   azs      = ["us-east-1a", "us-east-1b"]
 
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
-  #private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
+  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
   intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
 
   tags = {
@@ -36,10 +65,10 @@ module "vpc" {
   name = local.name
   cidr = local.vpc_cidr
 
-  azs             = local.azs
-  #private_subnets = local.private_subnets
-  public_subnets  = local.public_subnets
-  intra_subnets   = local.intra_subnets
+  azs              = local.azs
+  private_subnets  = local.private_subnets
+  public_subnets   = local.public_subnets
+  intra_subnets    = local.intra_subnets
 
   enable_nat_gateway = true
 
@@ -47,6 +76,9 @@ module "vpc" {
     "kubernetes.io/role/elb" = 1
   }
 
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
 }
 
 module "eks" {
@@ -59,24 +91,24 @@ module "eks" {
   cluster_addons = {
     coredns = {
       most_recent = true
+      # resolve_conflicts = "OVERWRITE" # <--- REMOVED THIS ARGUMENT
     }
     kube-proxy = {
       most_recent = true
+      # resolve_conflicts = "OVERWRITE" # <--- REMOVED THIS ARGUMENT
     }
     vpc-cni = {
       most_recent = true
+      # resolve_conflicts = "OVERWRITE" # <--- REMOVED THIS ARGUMENT
     }
   }
 
   vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.public_subnets
+  subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
-  # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
-    #ami_type       = "AL2_x86_64"
-    #instance_types = ["m5.large"]
-
+    ami_type = "AL2_x86_64"
     attach_cluster_primary_security_group = true
   }
 
@@ -94,19 +126,39 @@ module "eks" {
       }
     }
   }
-  
+
   manage_aws_auth_configmap = true
   aws_auth_roles = [
     {
-      rolearn  = module.eks.eks_managed_node_group_iam_role_arns["ascode-cluster-wg"] # Node group role
+      rolearn  = module.eks.eks_managed_node_group_iam_role_arns["ascode-cluster-wg"]
       username = "system:node:{{EC2PrivateDNSName}}"
       groups   = ["system:bootstrappers", "system:nodes"]
     },
     {
-      rolearn  = module.eks_admins_iam_role.iam_role_arn # <--- REPLACE WITH YOUR ACTUAL IAM USER ARN
-      username =  module.eks_admins_iam_role.iam_role_name
-      groups   = ["system:masters"] # Grants cluster-admin permissions
+      rolearn  = var.github_actions_runner_iam_role_arn # <--- CORRECTED THIS LINE
+      username = "github-actions-runner" # <--- CORRECTED THIS LINE (static string)
+      groups   = ["system:masters"]
     }
   ]
+
   tags = local.tags
+}
+
+# --- Variables for the root module ---
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "my-eks-cluster" # This default is overridden by local.name in this config
+}
+
+variable "github_actions_runner_iam_role_arn" {
+  description = "ARN of the IAM user/role used by GitHub Actions for EKS access"
+  type        = string
+  default = "arn:aws:iam::593793064016:user/Ammen"
 }

a66a76eeaf8b8a4205b321aa3bd96fd74ff40b60
Ameenrehman <ameen2607@gmail.com>
2025-07-05 18:16:05 +0530

    added github acction iam user arn

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,4 +12,6 @@ module "eks" {
   vpc_id       = module.vpc.vpc_id
   subnet_ids   = module.vpc.subnet_ids
   cluster_name = var.cluster_name
+  github_actions_runner_iam_role_arn = "arn:aws:iam::593793064016:user/Ammen"
+
 }

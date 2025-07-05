51622cac40467e517fb6652e9cf57c586e879aa6
Ameenrehman <ameen2607@gmail.com>
2025-07-05 16:29:19 +0530

    added two subet for eks

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -11,10 +11,17 @@ resource "aws_subnet" "subnet1" {
   map_public_ip_on_launch = true
 }
 
+resource "aws_subnet" "subnet2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
 output "vpc_id" {
   value = aws_vpc.main.id
 }
 
 output "subnet_ids" {
-  value = [aws_subnet.subnet1.id]
+  value = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
 }

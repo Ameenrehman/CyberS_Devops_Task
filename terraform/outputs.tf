f4b1e3db5876e655c1ab4207fa1a2aa4caa7b223
Ameenrehman <ameen2607@gmail.com>
2025-07-06 12:40:51 +0530

    added alb controller

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,8 @@ output "cluster_endpoint" {
 output "cluster_name" {
   value = module.eks.cluster_name
 }
+
+output "vpc_id" { # <--- ENSURE THIS IS PRESENT
+  description = "The ID of the VPC created by the module"
+  value       = module.vpc.vpc_id
+}
